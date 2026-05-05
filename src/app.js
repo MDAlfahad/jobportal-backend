5c4cfb85a525e66ea541b7ab08164a9317d260b9
MD Alfahad <alfahadkhan715@gmail.com>
2026-05-05 16:07:43 +0530

    fix: remove unnecessary semicolon in app.js

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const mailrouter = require("./routes/mailRoute");
 const uploadImage = require("./routes/photoUploadRoute");
 
 const app = express();
-app.use(express.json());
+app.use(express.json())
 
 app.use(cors({
   origin:"*",
