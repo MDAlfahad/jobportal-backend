b78285ae1c856ce8b2a6603ed97648e955ca8a9e
MD Alfahad <alfahadkhan715@gmail.com>
2026-05-12 17:28:20 +0530

    fix: remove userImages endpoint due to incorrect SQL query and response handling

--- src/routes/photoUploadRoute.js
+++ src/routes/photoUploadRoute.js
@@ -65,19 +65,6 @@ uploadImage.post(
   }
 );
 
-uploadImage.get("/userImages", (req, res)=>{
-  db.query(`SELECT user_image rom user_logindata WHERE user_id =?`, (err, result)=>{
-    res.status(500).json({
-      success: false,
-      message: "Database error"
-    })
-    res.status(200).json({
-      success:true,
-      Message: "Fetch successfuly"
-    })
-  })
-})
-
 
 uploadImage.post("/companyDetails", upload.single("image"), (req, res) => {
   const { address, number, userId } = req.body;
