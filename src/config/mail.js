a72fdac367f57d3e50ba0283e2738b49fce133f6
MD Alfahad <alfahadkhan715@gmail.com>
2026-05-12 14:28:03 +0530

    fix: streamline SMTP transporter configuration and improve error logging

--- src/config/mail.js
+++ src/config/mail.js
@@ -5,36 +5,24 @@ dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 587, 
+  secure: false, 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  family: 4,
   tls: {
     rejectUnauthorized: false,
   },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
 });
 
-
-transporter.verify((error, success) => {
-  if (error) {
-    console.log("SMTP ERROR:", error);
+transporter.verify((err, success) => {
+  if (err) {
+    console.log(err);
   } else {
-    console.log("SMTP SERVER READY");
+    console.log("SMTP Ready");
   }
 });
 
-const sendEmail = async (to, subject, html) => {
-  await transporter.sendMail({
-    from: `JOB PORTAL <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html,
-  });
-};
-
-module.exports = sendEmail; 
+module.exports = transporter;
