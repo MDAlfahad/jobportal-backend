693fd28df0009f868f8980123fe481a5c3b23e31
MD Alfahad <alfahadkhan715@gmail.com>
2026-05-12 15:27:41 +0530

    fix: update SMTP transporter configuration and enhance error logging

--- src/config/mail.js
+++ src/config/mail.js
@@ -6,20 +6,18 @@ dns.setDefaultResultOrder("ipv4first");
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: false,
-  family: 4,
+  secure: false, 
+  family: 4,    
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false,
-  },
+  }
+ 
 });
 
 transporter.verify((err, success) => {
   if (err) {
-    console.log("SMTP ERROR:", err);
+    console.error("SMTP ERROR:", err);
   } else {
     console.log("SMTP Ready");
   }
