fffebb86759e9ee16df232ce84c0322c3a7ba4e7
MD Alfahad <alfahadkhan715@gmail.com>
2026-05-12 14:22:36 +0530

    fix: enhance SMTP configuration and add connection verification for email transport

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,12 +1,31 @@
 const nodemailer = require("nodemailer");
+const dns = require("dns");
+
+dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
+});
+
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.log("SMTP ERROR:", error);
+  } else {
+    console.log("SMTP SERVER READY");
+  }
 });
 
 const sendEmail = async (to, subject, html) => {
@@ -18,4 +37,4 @@ const sendEmail = async (to, subject, html) => {
   });
 };
 
-module.exports = sendEmail;
+module.exports = sendEmail; 
